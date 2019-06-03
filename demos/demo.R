94f63091197109b7b548471c1afdbba07403d143
BelhalK <belhal.karimi@gmail.com>
2019-06-03 10:30:57 +0200

    maj

--- demos/demo.R
+++ demos/demo.R
@@ -24,7 +24,7 @@ saemix.model_warfa<-saemixModel(model=model1cpt,description="warfarin",type="str
 
 
 ###RTTE
-timetoevent.saemix <- read.table("data/rttellis.csv", header=T, sep=",")
+timetoevent.saemix <- read.table("data/tte.saemix.csv", header=T, sep=",")
 timetoevent.saemix <- timetoevent.saemix[timetoevent.saemix$ytype==2,]
 saemix.data_rtte<-saemixData(name.data=timetoevent.saemix,header=TRUE,sep=" ",na=NA, name.group=c("id"),name.response=c("y"),name.predictors=c("time","y"), name.X=c("time"))
 
