fae8147609e3584dbf3660bd05b4ebcd4ede49d5
BelhalK <belhal.karimi@gmail.com>
2019-09-09 18:14:54 +0200

    maj

--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,3 @@
 .onAttach <-function (lib, pkg) {
-  packageStartupMessage("     Loading package saemix, version 2.2, October 2018\n         please direct bugs, questions and feedback to belhal.karimi@inria.fr\n")
+  packageStartupMessage("     Loading package saemix, version 3.1, September 2019\n         please direct bugs, questions and feedback to belhal.karimi@inria.fr\n")
 }
